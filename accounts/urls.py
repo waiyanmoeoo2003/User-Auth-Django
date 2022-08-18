2eceaf1b74d43095b827c8c064c98ea06d72b0ab
Aung Kaung Myat <lordmuzan44252@gmail.com>
2022-08-18 21:59:28 +0630

    decorators

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path , include
 from . import views
 urlpatterns = [
     path('index',views.indexPage,name="index"),
+    path('admin',views.adminPage,name="admin"),
     path('login',views.loginPage,name="login"),
     path('logout',views.logoutUser,name="logout"),
 
--- accounts/views.py
+++ accounts/views.py
@@ -7,45 +7,55 @@ from django.contrib import messages
 
 from django.contrib.auth import authenticate , login , logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from .decorators import admin_only, unauthenticated_user , allowed_users
 # Create your views here.
 
 
 @login_required(login_url='login')
+@allowed_users(allowed_roles=['admin','customer'])
 def indexPage(request):
     context={}
     return render(request,'index.html',context)
 
-def loginPage(request):
-    if request.user.is_authenticated:
-        return redirect('index')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user=authenticate(request,username=username,password=password)
+@admin_only
+def adminPage(request):
+    context={}
+    return render(request,'admin.html',context)
 
-            if user is not None:
-                login(request,user)
-                return redirect('index')
-            else:
-                messages.info(request,'Username OR Password is incorrect')
+@unauthenticated_user
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            messages.info(request,'Username OR Password is incorrect')
     context={}
     return render(request,'login.html',context)
 
+@unauthenticated_user
 def registerPage(request):
-    if request.user.is_authenticated:
-        return redirect('index')
-    else:
-        form=CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            print(request)
-            if form.is_valid():
-                form.save()
-                user=form.cleaned_data.get('username')
-                messages.success(request,'Account was created for '+user)
-                return redirect('login')
-        
+
+    form=CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        print(request)
+        if form.is_valid():
+            user = form.save()
+            
+            username=form.cleaned_data.get('username')
+
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+            messages.success(request,'Account was created for '+username)
+            return redirect('login')
+    
     context={'form':form}
     return render(request,'register.html',context)
 
